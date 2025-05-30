e9a942fb2b431957eeeacdf54e74ebacc173ef69
L00188600 <L00188600@atu.ie>
2025-05-30 19:58:03 +0100

    Update preprocess.py

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,9 +1,7 @@
 #scripts/preprocess.py
 def preprocess_data(df):
-    """
-    Applies preprocessing steps to the input DataFrame.
-    Currently, only scales numerical features.
-    """
+    #Applies preprocessing steps to the input DataFrame.
+    #Currently, only scales numerical features.
     #Assuming the first four columns are numerical features for scaling
     #numerical_features = ['sepal_length', 'sepal_width',
     #                      'petal_length', 'petal_width']
