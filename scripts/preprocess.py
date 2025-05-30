35b9acd72ceb4283d35ddfb9686c4cb64749a347
L00188600 <L00188600@atu.ie>
2025-05-30 19:56:38 +0100

    Update preprocess.py

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,21 +1,13 @@
-# scripts/preprocess.py
-
-
+#scripts/preprocess.py
 def preprocess_data(df):
     """
     Applies preprocessing steps to the input DataFrame.
     Currently, only scales numerical features.
     """
-    # Assuming the first four columns are numerical features for scaling
-    # numerical_features = ['sepal_length', 'sepal_width',
-    #                       'petal_length', 'petal_width']
-
-    # Check if a scaler exists from previous training, if not, create a dummy
-   
-
+    #Assuming the first four columns are numerical features for scaling
+    #numerical_features = ['sepal_length', 'sepal_width',
+    #                      'petal_length', 'petal_width']
+    #Check if a scaler exists from previous training, if not, create a dummy
     return df
-
-
 if __name__ == '__main__':
-  
     print("Preprocessing script executed.")
